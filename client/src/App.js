4822ae210cfe06f7d3c8a6054c5497f8f0e8efb5
Tabalbar <Tabalbar@hawaii.edu>
2021-03-14 20:50:17 -1000

    added csv processor

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import 'semantic-ui-css/semantic.min.css'
 import { Button, Grid, Input } from 'semantic-ui-react'
 import { VegaLite } from 'react-vega'
 import MaterialTable from 'material-table'
+import XLSX from 'xlsx'
 
 function App() {
 
@@ -11,7 +12,7 @@ function App() {
 
   useEffect(() => {
 
-  },[dataFile])
+  }, [dataFile])
 
   const testingNode = async () => {
     const response = await fetch('http://localhost:5000/', {
@@ -99,33 +100,77 @@ function App() {
       rating: 4.36
     }
   ];
+
+  const processData = (data) => {
+    const dataStringLines = data.split(/\r\n|\n/);
+    const headers = dataStringLines[0].split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/);
+
+    const list = [];
+    for (let i = 1; i < dataStringLines.length; i++) {
+      const row = dataStringLines[i].split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/);
+      if (headers && row.length == headers.length) {
+        const obj = {};
+        for (let j = 0; j < headers.length; j++) {
+          let d = row[j];
+          if (d.length > 0) {
+            if (d[0] == '"')
+              d = d.substring(1, d.length - 1);
+            if (d[d.length - 1] == '"')
+              d = d.substring(d.length - 2, 1);
+          }
+          if (headers[j]) {
+            obj[headers[j]] = d;
+          }
+        }
+
+        // remove the blank rows
+        if (Object.values(obj).filter(x => x).length > 0) {
+          list.push(obj);
+        }
+      }
+    };
+    console.log(list)
+  }
   const laodData = (e) => {
     e.preventDefault()
-    console.log(e.target.files[0])
-    if(e.target.files[0] !== undefined) {
-      console.log('here')
+    const file = e.target.files[0]
+    if (file) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        // Use reader.result
+        const bstr = e.target.result;
+        const wb = XLSX.read(bstr, { type: 'binary' });
+        /* Get first worksheet */
+        const wsname = wb.SheetNames[0];
+        const ws = wb.Sheets[wsname];
+        /* Convert array of arrays */
+        const data = XLSX.utils.sheet_to_csv(ws, { header: 1 });
+        processData(data)
+      }
+      reader.readAsBinaryString(file);
+
     }
   }
 
-  return (
-    <>
-      <br /><br />
-      <Grid centered={true}>
-        <Grid.Row>
-          <Input placeholder={'...Enter query Here'} />
-          <Button onClick={testingNode}>Create Visualization</Button>
-          <Input type='file' onChange={laodData}/>
-        </Grid.Row>
-        <Grid.Row>
-          <VegaLite spec={spec} data={barData} />
-        </Grid.Row>
-        <Grid.Row>
-          <MaterialTable columns={columns} data={data} title='Books Directory' />
-        </Grid.Row>
-      </Grid>
+    return (
+      <>
+        <br /><br />
+        <Grid centered={true}>
+          <Grid.Row>
+            <Input placeholder={'...Enter query Here'} />
+            <Button onClick={testingNode}>Create Visualization</Button>
+            <Input type='file' onChange={laodData} />
+          </Grid.Row>
+          <Grid.Row>
+            <VegaLite spec={spec} data={barData} />
+          </Grid.Row>
+          <Grid.Row>
+            <MaterialTable columns={columns} data={data} title='Books Directory' />
+          </Grid.Row>
+        </Grid>
 
-    </>
-  );
-}
+      </>
+    );
+  }
 
-export default App;
+  export default App;
