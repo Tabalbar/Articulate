c0111557c41885e72b5c37c585e88c8b459b1b67
Tabalbar <35385849+Tabalbar@users.noreply.github.com>
2021-05-07 10:33:58 -1000

    added command to make graphs

--- client/src/App.js
+++ client/src/App.js
@@ -179,8 +179,8 @@ function App() {
     setErrMsg(prev => prev + "Returned " + count + " charts")
     tmpText += "Returned " + count + " chart(s)"
     
-    UseVoice(tmpText)
-
+    let utterance = UseVoice(tmpText)
+    return utterance
   }
 
 
@@ -252,10 +252,99 @@ export default App;
 const Dictaphone = ({
   createChartWithVoice
 }) => {
-  const { transcript, resetTranscript } = useSpeechRecognition()
+
+  const [listening, setListening] = useState(false)
+
+  let commands = [
+    {
+      command: "computer *",
+      callback: (command) => {
+        console.log('listening')
+        let utterance = createChartWithVoice(command)
+        utterance.onend = function(event) {
+          console.log('Utterance has finished being spoken after ' + event.elapsedTime + ' milliseconds.');
+          setListening(true)
+        }
+      }
+    },
+    {
+      command: "computer",
+      callback: () => {
+        let utterance = UseVoice("At your service")
+        utterance.onend = function(event) {
+
+          console.log('Utterance has finished being spoken after ' + event.elapsedTime + ' milliseconds.');
+          setTimeout(() => {
+            setListening(true)
+            console.log('listening')
+          }, 2500)
+
+
+        }
+      }
+    },
+    {
+      command: "computer are you there?",
+      callback: () => {
+        let utterance = UseVoice("Yes, how can i help you?")
+        utterance.onend = function(event) {
+          console.log('Utterance has finished being spoken after ' + event.elapsedTime + ' milliseconds.');
+          setListening(true)
+        }
+      }
+    },
+    {
+      command: "*",
+      callback: (command) => {
+        if(listening){
+          console.log('executing')
+          let utterance = createChartWithVoice(command)
+          utterance.onend = function(event) {
+            console.log('Utterance has finished being spoken after ' + event.elapsedTime + ' milliseconds.');
+            setListening(true)
+          }
+        }
+      }
+    }
+  ]
+
+  useEffect(() => {
+    if(listening) {
+      const timer = setTimeout(() => {
+        setListening(false)
+        console.log('not listening')
+      }, 10000)
+      return () => {
+        clearTimeout(timer)
+      }
+    }
+
+  }, [listening])
+
+  // if(listening) {
+  //   // console.log('listening')
+  //   commands = [
+  //     {
+  //       command: "*",
+  //       callback: (command) => {
+  //         let commandUtterance = createChartWithVoice(command)
+  //         console.log(command)
+  //         setListening(true)
+  //         commandUtterance.onend = function(event) {
+  //           console.log('Utterance has finished being spoken after ' + event.elapsedTime + ' milliseconds.');
+  //         }
+  //       }
+  //     }
+  //   ]
+  // }
+
+  const { transcript, resetTranscript } = useSpeechRecognition({ commands})
 
   if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
     return null
+  } else {
+    SpeechRecognition.startListening({ continuous: true })
+
   }
 
   return (
--- client/src/UseVoice.js
+++ client/src/UseVoice.js
@@ -5,9 +5,9 @@ function UseVoice(text) {
     msg.volume = 1; // From 0 to 1
     msg.rate = 1; // From 0.1 to 10
     msg.pitch = 0; // From 0 to 2
-    msg.lang = 'en';
     msg.text = text
     window.speechSynthesis.speak(msg);
+    return msg
 }
 
 export default UseVoice
