ec2aa1b8d40e891489dba587e7a792817e844e7c
Tabalbar <35385849+Tabalbar@users.noreply.github.com>
2021-06-05 17:53:17 -1000

    removed special charts from neural network

--- routes/index.js
+++ routes/index.js
@@ -22,80 +22,39 @@ manager.addDocument('en', 'I want to see quantitative by quantitative', 'scatter
 manager.addDocument('en', 'show me quantiative by quantitative', 'scatter');
 manager.addAnswer('en', 'scatter', 'scatter');
 
-manager.addDocument('en', 'show me the composition of quantitative', 'pie');
-manager.addDocument('en', 'I want to see the quantitative and nominal', 'pie');
-manager.addDocument('en', 'show me the percentage of quantitative and nominal', 'pie');
-manager.addAnswer('en', 'pie', 'pie');
-
-manager.addDocument('en', 'can you show me a marginal heat map', 'marginalHistogram');
-manager.addDocument('en', 'I want to see the distribution of quantitative and quantitative', 'marginalHistogram');
-manager.addDocument('en', 'show me a graph with extra bars on the side', 'marginalHistogram');
-manager.addDocument('en', 'show me a heat map of quantitative and quantitative with bar charts on the side', 'marginalHistogram');
-manager.addAnswer('en', 'marginalHistogram', 'marginalHistogram');
-
 manager.addDocument('en', 'show me the distribution of quantitative and quantitative', 'heatmap');
 manager.addDocument('en', 'show me a 2D heatmap', 'heatmap');
 manager.addAnswer('en', 'heatmap', 'heatmap');
 
-manager.addDocument('en', 'Show me the area under the curve for temporal quantitative and temoral', 'lineArea');
-manager.addDocument('en', 'show me the quantitative and nominal over time', 'lineArea');
-manager.addAnswer('en', 'lineArea', 'lineArea');
-
-// manager.addDocument('en', 'show me a normalized graph with quantitative and nominal over time', 'normalizedLineArea');
-// manager.addDocument('en', 'show me a normalized of temporal quantitative and nominal ', 'normalizedLineArea');
-// manager.addAnswer('en', 'normalizedLineArea', 'normalizedLineArea');
-
-manager.addDocument('en', 'show me a stacked bar', 'stackedBar');
-manager.addAnswer('en', 'stackedBar', 'stackedBar');
-
-// manager.addDocument('en', 'show me a normalized stacked bar chart of ', 'normalizedStackedBar');
-// manager.addDocument('en', 'show me a normalized stacked bar chart of temporal quantitative and nominal', 'normalizedStackedBar');
-// manager.addAnswer('en', 'normalizedStackedBar', 'normalizedStackedBar');
-
-// manager.addDocument('en', 'show me the stock trend', 'candleStick');
-// manager.addAnswer('en', 'candleStick', 'candleStick');
-
 manager.addDocument('en', 'I want to see the difference of nominal by quantitative quantitative and quantitative', 'parallelCoordinates');
 manager.addAnswer('en', 'parallelCoordinates', 'parallelCoordinates');
-
-manager.addDocument('en', 'show me a radar graph of temporal and quantitative', 'radar');
-manager.addAnswer('en', 'radar', 'radar');
-
-// manager.addDocument('en', 'this but with ', 'filter');
-// manager.addAnswer('en', 'filter', 'filter');
 
 // Train and save the model.
 (async () => {
   await manager.train();
   manager.save();
-  const response = await manager.process('en', 'I should go now');
 })();
 
 const chartMaker = require('../chartMaker/chartMaker')
 const createVector = require('../chartMaker/createVector')
 const normalizeCommand = require('../chartMaker/normalizeCommand')
 const generalizeCommand = require('../chartMaker/generalizeCommand')
-const iterateGraph = require('../chartMaker/iterateGraph')
 const countVector = require('../chartMaker/countVector')
 const ExplicitChart = require('../chartMaker/specifications/ExplicitChart')
 const chartOptions = require('../chartMaker/specifications/chartOptions')
-const testPCA = require('../chartMaker/testPCA')
 
 // const findommands = require('../chartMaker/findCommands')
 const nlp = require('compromise')
 
 /* GET home page. */
 router.post('/', async (req, res, next) => {
-  let specs = [];
   const data = req.body.dataHead;
   const attributes = req.body.attributes
   const transcript = req.body.overHearingData
-  const prevChart = req.body.prevChart
   const synonymAttributes = req.body.synonymAttributes
   const featureAttributes = req.body.featureAttributes
   const randomChart = req.body.randomChart
   const currentCharts = req.body.currentCharts
-  let charts = []
   const command = req.body.command
   const normalizedCommand = normalizeCommand(command)
   const { generalizedCommand, synonymCommand } = generalizeCommand(normalizedCommand, attributes, data)
@@ -105,20 +64,7 @@ router.post('/', async (req, res, next) => {
   const headerMatrix = createVector(attributes, data)
   const { headerFreq } = countVector(transcript, featureAttributes, synonymAttributes, data)
 
-  // testPCA(data)
 
-  nlp.extend((Doc, world) => {
-    const headers = req.body.headers
-    // add methods to run after the tagger
-    world.postProcess(doc => {
-      headerMatrix.forEach(firstD => {
-        firstD.forEach(noun => {
-          doc.match(noun).tag('#Noun')
-          doc.match(noun + 's').tag('#Noun')
-        })
-      });
-    })
-  })
   if(randomChart) {
     explicitChart = chartOptions[Math.floor(Math.random() * chartOptions.length)].mark
   }
@@ -165,6 +111,8 @@ router.post('/addHeaders', async (req, res, next) => {
     world.postProcess(doc => {
       nlpHeaders.forEach(header => {
         doc.match(header).tag('#Noun')
+        doc.match(noun + 's').tag('#Noun')
+
       });
     })
   })
